adda95889ed72e0de6f27f74f0b6903f36e99881
BioStarts <ipyo.ipa@gmail.com>
2018-12-25 23:13:51 +0300

    separated the rigidly connected interface from working with the network (added Network class and functional interface Callback)  use Strategy Pattern

--- client/src/main/java/com/geekbrains/client/Controller.java
+++ client/src/main/java/com/geekbrains/client/Controller.java
@@ -35,10 +35,6 @@ public class Controller implements Initializable {
     private boolean authentificated;
     private String nickname;
 
-    private Socket socket;
-    private DataInputStream in;
-    private DataOutputStream out;
-
     public void setAuthentificated(boolean authentificated) {
         this.authentificated = authentificated;
         authPanel.setVisible(!authentificated);
@@ -47,114 +43,39 @@ public class Controller implements Initializable {
         msgPanel.setManaged(authentificated);
         clientsList.setVisible(authentificated);
         clientsList.setManaged(authentificated);
-        if(!authentificated){
+        if (!authentificated) {
             nickname = ""; // обнуляем ник клиента при разрыве связи
         }
     }
 
-    public void connect() {
-        try {
-            setAuthentificated(false);
-            socket = new Socket("localhost", 8189);
-            in = new DataInputStream(socket.getInputStream());
-            out = new DataOutputStream(socket.getOutputStream());
-            Thread t = new Thread(() -> {
-                try {
-                    while (true) {//ждем потдверждения от сервака что мы ок, т.е. есть в базе (квазитокен)
-                        String msg = in.readUTF();
-                        if(msg.startsWith("/authok ")) {
-                            setAuthentificated(true);
-                            nickname = msg.split("\\s")[1];
-                            break;
-                        }
-                    }
-                    while (true) {//все ок мы норм, ждем сообщений
-                        String msg = in.readUTF();
-                        if (msg.startsWith("/")) { //говорим что если летит что-то начинающиеся на / это команда
-                            if (msg.startsWith("/clients ")){
-                                Platform.runLater(() -> {// прокидывает задачу по обновлению списка из этого трэда в трэд javaFX
-                                    clientsList.getItems().clear();//отчистили старый список клиентов
-                                    String[] tokens = msg.split("\\s");//парсим список заполняем массив листа(списка)
-                                    for (int i = 1; i < tokens.length ; i++) {
-                                        clientsList.getItems().add(tokens[i]);
-                                    }
-                                });
-                            }
-                            if (msg.equals("/end")){//если от сервака летит /end - летим в finaly минуя алерт о разрыве соедениния(т.к. сами его прервали командой /енд)
-                                break;
-                            }
-                            continue; //таким образом ушли от уровня вложенности else
-                        }
-                        textArea.appendText(msg + "\n");
-                    }
-                } catch (IOException e){
-                    showAlert("Соединение с сервером разорвано"); //кидаем алерт что соедение с сервером разорвано
-                } finally {
-                    closeConnection();
-                }
-            });
-            t.setDaemon(true);
-            t.start();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public void sendAuth() {
+        Network.sendAuth(loginField.getText(), passField.getText());
+        loginField.clear();
+        passField.clear();
     }
 
-    public void sendMsg(ActionEvent actionEvent) {
-        try {
-            out.writeUTF(msgField.getText());
+
+    public void sendMsg() {
+        if (Network.sendMsg(msgField.getText())) {//если отправка прошла то очищаем поле и кидаем фокус на поле
             msgField.clear();
             msgField.requestFocus();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-    }
-
-    public void sendAuth() {
-        try {
-            if(socket == null || socket.isClosed()){ // при разрыве соединения переподключаемся
-                connect();
-            }
-            out.writeUTF("/auth " + loginField.getText() + " " + passField.getText());
-            loginField.clear();
-            passField.clear();
-        } catch (IOException e) {
-            e.printStackTrace();
         }
     }
 
-    public void closeConnection(){
-        setAuthentificated(false);
-        try {
-            in.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        try {
-            out.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        try {
-            socket.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         setAuthentificated(false);
         //повешали на список прослушку события что кто-то на него кликает
         clientsList.setOnMouseClicked(event -> {
-            if (event.getClickCount() == 2){
+            if (event.getClickCount() == 2) {
                 String nickname = clientsList.getSelectionModel().getSelectedItem();
                 msgField.setText("/w " + nickname + " ");
                 msgField.requestFocus();
                 msgField.selectEnd();
             }
         });
+        linkCallbacks();
     }
 
     public void showAlert(String msg) {//метод для бросания алертов
@@ -162,5 +83,30 @@ public class Controller implements Initializable {
             Alert alert = new Alert(Alert.AlertType.WARNING, msg, ButtonType.OK);
             alert.showAndWait();
         });
+    }
+
+    public void linkCallbacks() {
+        Network.callOnException = args -> showAlert(args[0].toString());
+        Network.callOnCloseConnection = args -> setAuthentificated(false);
+        Network.callOnAuthentificated = args -> {
+            setAuthentificated(true);
+            nickname = args[0].toString();
+        };
+        Network.callOnMsgReceived = args -> {
+            String msg = args[0].toString();
+            if (msg.startsWith("/")) { //говорим что если летит что-то начинающиеся на / это команда
+                if (msg.startsWith("/clients ")) {
+                    Platform.runLater(() -> {// прокидывает задачу по обновлению списка из этого трэда в трэд javaFX
+                        clientsList.getItems().clear();//отчистили старый список клиентов
+                        String[] tokens = msg.split("\\s");//парсим список заполняем массив листа(списка)
+                        for (int i = 1; i < tokens.length; i++) {
+                            clientsList.getItems().add(tokens[i]);
+                        }
+                    });
+                }
+            } else {
+                textArea.appendText(msg + "\n");
+            }
+        };
     }
 }
